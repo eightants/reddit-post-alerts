482f571ef0551b7d91a355b77929da0eb30230bf
Anthony <yihonganthony@gmail.com>
2019-09-27 13:48:08 -0500

    changed settings for bots and longer wait time for checks

--- post-alert-bot/RedditScrape.py
+++ post-alert-bot/RedditScrape.py
@@ -5,32 +5,27 @@ import os
 import time
 from datetime import datetime, timedelta
 
-subs = ['spotify']
-keywords = ['player', 'playlist']
 
-def ScrapePosts(subs, keywords):
+def ScrapePosts(sub, keywords):
     posts = []
-    for sub in subs:
-        try:
+    try:
+        username = os.environ['BOTONE_USERNAME']
+        password = os.environ['BOTONE_PASSWORD']
+        client_id = os.environ['BOTONE_ID']
+        client_secret = os.environ['BOTONE_SECRET']
+        user_agent = os.environ['BOTONE_AGENT']
 
-            username = os.environ['BOTONE_USERNAME']
-            password = os.environ['BOTONE_PASSWORD']
-            client_id = os.environ['BOTONE_ID']
-            client_secret = os.environ['BOTONE_SECRET']
-            user_agent = os.environ['BOTONE_AGENT']
-            
-            reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, username=username, user_agent=user_agent)
-            #reddit = praw.Reddit(client_id="ffyfOn7R40octw", client_secret="eQ_8GAD7K-oUeA8W44anLmEn1l0", password="thevacation", username="askreddit-sim", user_agent="redditscrape-v1")
+        reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, username=username, user_agent=user_agent)
 
-            subreddit = reddit.subreddit(sub)
+        subreddit = reddit.subreddit(sub)
 
-            # checks for any new post
-            for submission in subreddit.new(limit=5):
+        # checks for any new post
+        for submission in subreddit.new(limit=5):
                 for keyword in keywords:
                     if submission.title.lower().find(keyword) != -1: 
                         posts.append(submission)
                         break
-            time.sleep(5)            
-        except Exception as e: 
-            time.sleep(10)
+        time.sleep(5)            
+    except Exception as e: 
+        time.sleep(10)
     return posts
