d81346820235e82a2f43736e7ab7d939e69dd71b
Anthony <yihonganthony@gmail.com>
2019-09-29 15:35:54 -0500

    Added postgresql db

--- post-alert-bot/bot.py
+++ post-alert-bot/bot.py
@@ -6,6 +6,7 @@ import random
 import asyncio
 import discord
 import RedditScrape as rs
+import psycopg2
 
 # read config file
 subs = ['frugalmalefashion']
@@ -13,6 +14,9 @@ keywords = ['adidas', 'ultraboost', 'uniqlo']
 channelid = 627214659719790594
 
 token = os.environ['DISCORDBOT_TOKEN']
+DATABASE_URL = os.environ['DATABASE_URL']
+
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 client = discord.Client()
 
@@ -22,13 +26,25 @@ async def on_ready():
     channel = client.get_channel(channelid)
     print(channel)
     # await client.wait_until_ready()
-    while True:
-        for sub in subs:
-            posts = rs.ScrapePosts(sub, keywords)
-            for p in posts:
+
+    # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
+    cur = conn.cursor()
+
+    for sub in subs:
+        posts = rs.ScrapePosts(sub, keywords)
+        for p in posts:
+            # Executing your PostgreSQL query
+            cur.execute("SELECT EXISTS (SELECT 1 FROM reddit-post-alerts WHERE post_id=" + str(p.id) + ")")
+            post_id = cur.fetchone()[0]
+            if post_id == False: 
+                cur.execute("INSERT INTO reddit-post-alerts (post_id) VALUES (" + p.id + ")")
+                # In order to make the changes to the database permanent, we now commit our changes
+                conn.commit()
                 await channel.send("[" + sub + "] " + p.title + "\n" + p.url)
-                #await channel.send(p.url)
-        await asyncio.sleep(1200)
+            #await channel.send(p.url)
+    # We have committed the necessary changes and can now close out our connection
+    cur.close()
+    conn.close()
 
 
 client.run(token)
