c0049e6460d100c133dca60a3c51ba7b1af7f898
Anthony <yihonganthony@gmail.com>
2019-09-29 19:15:42 -0500

    readded frequent checking

--- post-alert-bot/bot.py
+++ post-alert-bot/bot.py
@@ -26,26 +26,26 @@ async def on_ready():
     channel = client.get_channel(channelid)
     print(channel)
     # await client.wait_until_ready()
-
-    # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
-    cur = conn.cursor()
-
-    for sub in subs:
-        posts = rs.ScrapePosts(sub, keywords)
-        for p in posts:
-            # Executing your PostgreSQL query
-            cur.execute("SELECT EXISTS (SELECT 1 FROM redditpostalerts WHERE post_id = '" + str(p.id) + "');")
-            post_id = cur.fetchone()[0]
-            print(post_id)
-            if post_id == False: 
-                cur.execute("INSERT INTO redditpostalerts (post_id) VALUES ('" + p.id + "');")
-                # In order to make the changes to the database permanent, we now commit our changes
-                conn.commit()
-                await channel.send("[" + sub + "] " + p.title + "\n" + p.url)
-            #await channel.send(p.url)
-    # We have committed the necessary changes and can now close out our connection
-    cur.close()
-    conn.close()
+    while True: 
+        # Creating a cursor (a DB cursor is an abstraction, meant for data set traversal)
+        cur = conn.cursor()
+        for sub in subs:
+            posts = rs.ScrapePosts(sub, keywords)
+            for p in posts:
+                # Executing your PostgreSQL query
+                cur.execute("SELECT EXISTS (SELECT 1 FROM redditpostalerts WHERE post_id = '" + str(p.id) + "');")
+                post_id = cur.fetchone()[0]
+                print(post_id)
+                if post_id == False: 
+                    cur.execute("INSERT INTO redditpostalerts (post_id) VALUES ('" + p.id + "');")
+                    # In order to make the changes to the database permanent, we now commit our changes
+                    conn.commit()
+                    await channel.send("[" + sub + "] " + p.title + "\n" + p.url)
+                #await channel.send(p.url)
+        # We have committed the necessary changes and can now close out our connection
+        cur.close()
+        conn.close()
+        await asyncio.sleep(300)
 
 
 client.run(token)
