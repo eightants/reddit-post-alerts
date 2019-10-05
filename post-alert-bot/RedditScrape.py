74f25a3c6c9a4dfbbbd48afd0663ae400a03f22e
Anthony <yihonganthony@gmail.com>
2019-10-05 18:21:00 -0500

    added redditscrape script

---
+++ post-alert-bot/RedditScrape.py
@@ -0,0 +1,32 @@
+import praw
+import pdb
+import re
+import os
+import time
+from datetime import datetime, timedelta
+
+
+def ScrapePosts(sub, keywords):
+    posts = []
+    try:
+        username = os.environ['BOTONE_USERNAME']
+        password = os.environ['BOTONE_PASSWORD']
+        client_id = os.environ['BOTONE_ID']
+        client_secret = os.environ['BOTONE_SECRET']
+        user_agent = os.environ['BOTONE_AGENT']
+
+        reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, username=username, user_agent=user_agent)
+
+        subreddit = reddit.subreddit(sub)
+
+        # checks for any new post
+        for submission in subreddit.new(limit=10):
+                for keyword in keywords:
+                    if submission.title.lower().find(keyword) != -1: 
+                        posts.append(submission)
+                        print("Match for" + keyword + ": " + submission.title)
+                        break
+        time.sleep(2)            
+    except Exception as e: 
+        time.sleep(10)
+    return posts
