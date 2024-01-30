6e2ff517bf9545274f704c78caece10db0bcd452
samlaujw <samlaujw@gmail.com>
2024-01-30 15:30:06 -0500

    update bot subs

--- post-alert-bot/bot.py
+++ post-alert-bot/bot.py
@@ -13,19 +13,11 @@ from lxml import html
 from selenium import webdriver
 from time import sleep
 
-# 11 for spring, 21 for summer, 31 for fall
-#term = "201911"
-#crn = "12359"
-#surl = "https://compass-ssb.tamu.edu/pls/PROD/bwykschd.p_disp_detail_sched?term_in=" + term + "&crn_in=" + crn
+#channelid = 627214659719790594
+subs = ['mangaswap']
+keywords = ['Buy', 'Sell', 'Trade', 'Free', 'Closed']
 
-
-#subs = ['frugalmalefashion', 'freebies']
-#keywords = [['adidas', 'ultraboost', 'uniqlo', 'nike', 'stan smith', 'a&f', 'abercrombie'], ['amazon']] #old keywords: alphabounce, vans
-channelid = 627214659719790594
-subs = ['buildapcsales', 'freebies']
-keywords = [['vp249q', 'vg249q', '24g2', '27g2', '2x8gb', '2 x 8gb', 'sn750', 'h510', 'seasonic', 's12iii', 'rx 5600 xt', 'gtx 1650', 'gtx 1660', 'gtx 2060', 'rx 5700'], ['amazon']]
-
-token = os.environ['DISCORDBOT_TOKEN']
+#token = os.environ['DISCORDBOT_TOKEN']
 DATABASE_URL = os.environ['DATABASE_URL']
 
 client = discord.Client()
